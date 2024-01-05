15950ef1c85700b12842f3d09aac5e8877024906
kstep99 <kstep99@outlook.com>
2024-01-05 10:11:14 -0600

    var lessons_ch2

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,1 +1,22 @@
-alert("hello, world");
+// alert("hello, world");
+
+// console.log(1);
+// console.log(2);
+
+//const and let are new to js and the main ways to make variables, const cant be reassigned
+let age = 25;
+let year = 2019
+console.log(age, year);
+
+
+//reset a variables value linke this
+age = 30;
+console.log(age);
+
+const points = 100;
+//points = 50;
+console.log(points);
+
+//there is an older way to make variables and it is with var keyword
+var score = 75;
+console.log(score);
