b92549eb1f0e30102bbded5035f9673e9e588e52
kstep99 <kstep99@outlook.com>
2024-01-05 13:27:14 -0600

    template strings over, boleans start js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -129,3 +129,25 @@
 //This is Template Strings:
 
 
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
+
+//TEMPLATE STRING uses backticks!
+// template string way
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
+
+console.log(result);
+
+// creating html templates
+let html = `
+  <h2>${title}</h2>
+  <p>By ${author}</p>
+  <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
